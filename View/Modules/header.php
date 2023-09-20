6937624533201f7fdec3cb431b6049cb0663cda1
Axel Sierra <77809543+axelsierra7@users.noreply.github.com>
2023-09-20 15:38:54 -0600

    Primeros Cambios en Header incluyendo el menu

--- View/Modules/header.php
+++ View/Modules/header.php
@@ -1,14 +1,23 @@
 <header>
-    <img class="logo" src="./img/LOGO1.png" alt="Logo">
-    <div class="barra1"></div>
+    <div class="barra-cabecera">
+        <ul class="menu">
+            <a href="./login.php"><img alt=""><i class="fa-solid fa-bars fa-2xl"></i></a>
+            <a href="./login.php"><img alt=""><i class="fa-solid fa-book-bookmark fa-2xl"></i></a>
+            <img class="logo" src="View/Resources/Img/LOGO1.png" alt="Logo">
 
-    <ul class="menu">
-        <li><a href="./login.php">Mi cuenta</a></li>
-        <li><a href="./catalogo.html">Catalogo</a></li>
 
 
-    </ul>
-    <a href="./login.php"><img class="carrito" src="./img/carrito.png" alt=""></a>
-    <a href="./login.php"><img class="auto" src="./img/auto.png" alt=""></a>
 
+
+
+
+            <a href="./login.php"><img class="carrito" src="" alt="">
+                <i class="fa-solid fa-cart-shopping fa-2xl"></i>
+            </a>
+            <a href="./login.php"><img class="auto" src="View/Resources/Img/" alt="">
+                <i class="fa-solid fa-car fa-2xl"></i>
+            </a>
+        </ul>
+
+    </div>
 </header>
